2ca33d199400c85de4c5636939002971fa0dd1e2
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2021-02-02 11:05:42 +0800

    Fix form 1

--- src/app/eggdonation/eggdonation.component.ts
+++ src/app/eggdonation/eggdonation.component.ts
@@ -9,39 +9,41 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EggdonationComponent implements OnInit {
 
   showForm: boolean = true;
+  registered_BIA = new FormControl('', [Validators.required]);
+  eye_color = new FormControl('', [Validators.required]);
+  hair_color = new FormControl('', [Validators.required]);
+  bloodType = new FormControl('', [Validators.required]);
+  medications = new FormControl('', [Validators.required]);
+  highest_level_education = new FormControl('', [Validators.required]);
 
   form: FormGroup = new FormGroup ({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
-    streetAdd: new FormControl('', Validators.required),
-    city: new FormControl('', Validators.required),
-    state: new FormControl(0, Validators.required),
-    outside_USA: new FormControl('', Validators.required),
+    streetAdd: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(0),
+    state_fill: new FormControl(''),
+    outside_USA: new FormControl(''),
     city_country_residence: new FormControl('', Validators.required),
-    zip_code: new FormControl('', Validators.required),
+    zip_code: new FormControl(''),
     telephone_number: new FormControl('', Validators.required),
-    contact_method: new FormControl(0, Validators.required),
-    others_contact_method: new FormControl('', Validators.required),
+    contact_method: new FormControl(0),
+    others_contact_method: new FormControl(''),
     birthDate: new FormControl('', Validators.required),
-    experience: new FormControl(0, Validators.required),
-    race: new FormControl('', Validators.required),
-    registered_BIA: new FormControl(0, Validators.required),
-    height: new FormControl('', Validators.required),
+    experience: new FormControl(0),
+    race: new FormControl(''),
+    height_feet: new FormControl('', Validators.required),
+    height_in: new FormControl('', Validators.required),
     weight: new FormControl('', Validators.required),
-    eye_color: new FormControl(0, Validators.required),
-    hair_color: new FormControl(0, Validators.required),
-    bloodType: new FormControl(0, Validators.required),
-    medications: new FormControl(0, Validators.required),
-    why_date_last_used: new FormControl('', Validators.required),
-    bioChildren: new FormControl('', Validators.required),
-    list_ages_children: new FormControl('', Validators.required),
-    hear_about_us: new FormControl(0, Validators.required),
-    explain_about_us: new FormControl('', Validators.required),
-    highest_level_education: new FormControl(0, Validators.required),
-    explain_other: new FormControl('', Validators.required),
-    yes_positive_carrier: new FormControl('', Validators.required),
-    preferred_languange: new FormControl(0, Validators.required),
-    others_languange: new FormControl('', Validators.required)
+    why_date_last_used: new FormControl(''),
+    bioChildren: new FormControl(''),
+    list_ages_children: new FormControl(''),
+    hear_about_us: new FormControl(0),
+    explain_about_us: new FormControl(''),
+    explain_other: new FormControl(''),
+    yes_positive_carrier: new FormControl(''),
+    preferred_languange: new FormControl(0),
+    others_languange: new FormControl('')
   });
 
   onSubmit() {
